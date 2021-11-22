de489f87cdd2d4535853fe58c13c451f4bd7cc55
Mary Gwen Susa <Mary Gwen G. Susa>
2021-11-22 12:07:17 +0800

    While function for 4 times repetition input function

--- Number Sorter.py
+++ Number Sorter.py
@@ -1,3 +1,6 @@
 print("\33[95m Number Sorter \33[00m")
 highest = secondHighest = secondLowest = lowest = 0 # first initialization (for creating variables with changing values)
 rep = 1 # second initialization
+while rep <= 4: # condition for one input line taken but can have specific repetition
+    rep += 1 # increment for starting number of repetition of input function
+
