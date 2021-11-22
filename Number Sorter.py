4920d67bbdc2c530b577bd447bb21f9f71a71d11
Mary Gwen Susa <Mary Gwen G. Susa>
2021-11-22 12:03:12 +0800

    Second initialization

--- Number Sorter.py
+++ Number Sorter.py
@@ -1,2 +1,3 @@
 print("\33[95m Number Sorter \33[00m")
-highest = secondHighest = secondLowest = lowest = 0 # first initialization
+highest = secondHighest = secondLowest = lowest = 0 # first initialization (for creating variables with changing values)
+rep = 1 # second initialization
