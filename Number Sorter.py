012dec377a3eed6c929fc71b80f25202aab402ec
Mary Gwen Susa <Mary Gwen G. Susa>
2021-11-22 14:23:52 +0800

    Display fixed prog 1 number sorter

--- Number Sorter.py
+++ Number Sorter.py
@@ -5,7 +5,7 @@ rep = 1 # second initialization
 """
 while rep <= 4: # condition for one input line taken but can have specific repetition
     try: # input validation
-        inputNumber = float(input("Enter number " + str(rep) + ": "))
+        inputNumber = float(input("\033[93m ENTER NUMBER" + str(rep) + ": "))
     except ValueError as alpha:
         print(f"\033[41m WARNING: \033[00m \033[96m You did not type in number format.\033[00m \033[31m{alpha}\033[00m")
         continue # loop
@@ -30,6 +30,5 @@ while rep <= 4: # condition for one input line taken but can have specific repet
         elif inputNumber > lowest:
             lowest = inputNumber
     rep += 1 # increment for number sequence of input function repetition
-lse:
-    print(f"\033[92m The order from \033[00m \033[94m HIGHEST \033[00m \033[92m to \033[00m \033[93m lowest \033[00m  \033[92m are \33[95m{highest: .2f},\33[00m \033[91m{secondHighest: .2f},\33[00m \33[93m{secondLowest: .2f},\33[00m \033[92m and \33[00m \033[91m{lowest: .2f}.\033[00m")
-
+else:
+    print(f"\033[00m \033[92m The order from \033[00m \033[94m HIGHEST \033[00m \033[92m to \033[00m \033[93m lowest \033[00m  \033[92m are \33[95m{highest: .2f},\33[00m \033[91m{secondHighest: .2f},\33[00m \33[93m{secondLowest: .2f},\33[00m \033[92m and \33[00m \033[91m{lowest: .2f}.\033[00m")
