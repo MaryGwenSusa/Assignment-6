918910fff80c16bb27c3c0dd26257f5797c0d0bf
Mary Gwen Susa <Mary Gwen G. Susa>
2021-11-22 12:26:54 +0800

    If condition function for considering the first initialization value

--- Number Sorter.py
+++ Number Sorter.py
@@ -5,7 +5,10 @@ while rep <= 4: # condition for one input line taken but can have specific repet
     try: # input validation
         inputNumber = float(input("Enter number " + str(rep) + ": "))
     except ValueError as alpha:
-        print(f"\033[41m WARNING: \033[00m \033[96m You did not type in number format.\033[00m {alpha}")
+        print(f"\033[41m WARNING: \033[00m \033[96m You did not type in number format.\033[00m \033[31m{alpha}\033[00m")
+        continue
+    if inputNumber <= -1: # consider first initialization is zero
+        print("\033[42m I'm sorry, I don't understand numbers below 1.\033[00m \033[93m Please input other numbers.\033[00m")
         continue
     rep += 1 # increment for starting number of repetition of input function
     
