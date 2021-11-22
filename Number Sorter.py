bbae2ea39aa2ab498a37a346b15e92935e89d8ad
Mary Gwen Susa <Mary Gwen G. Susa>
2021-11-22 12:24:52 +0800

    Input validation with printed statement plus continue function for looping

--- Number Sorter.py
+++ Number Sorter.py
@@ -2,8 +2,11 @@ print("\33[95m Number Sorter \33[00m")
 highest = secondHighest = secondLowest = lowest = 0 # first initialization (for creating variables with changing values)
 rep = 1 # second initialization
 while rep <= 4: # condition for one input line taken but can have specific repetition
-    inputNumber = float(input("Enter number " + str(rep) + ": "))
-    
+    try: # input validation
+        inputNumber = float(input("Enter number " + str(rep) + ": "))
+    except ValueError as alpha:
+        print(f"\033[41m WARNING: \033[00m \033[96m You did not type in number format.\033[00m {alpha}")
+        continue
     rep += 1 # increment for starting number of repetition of input function
     
 
