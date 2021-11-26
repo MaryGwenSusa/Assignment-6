94ad29f208c77c8329e5c3d7ac7c6345ded9cf89
Mary Gwen Susa <Mary Gwen G. Susa>
2021-11-26 13:34:43 +0800

    Defined function for question and answer input evaluation

--- Addition Quiz.py
+++ Addition Quiz.py
@@ -4,3 +4,12 @@ def header():
     print("*" * len(title)) # created a header design
     print(title)
     print("*" * len(title))
+
+def determineAns():
+    answer = digit1 + digit2 
+    equation = int(input ("What is %s + %s? \033[94m" % (digit1, digit2)))
+    if answer == equation:
+        print("\033[00m\33[95mYou are doing great! I am proud of you <<<333\33[00m")
+        return True
+    else:
+        print("\033[92mPlease, try again! ^_^\033[00m")
